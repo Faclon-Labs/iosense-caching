7173f5fb0c41d740d9baebfb69959f2629c5d04e
dhrutim <dhruti.m@iosense.io>
2023-02-27 12:12:29 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name = "iosense",
+    version = "0.0.1",
+    author = "Dhruti Mistry",
+    description = "iosense data fetching library",
+    packages = ["iosense"]
+)
