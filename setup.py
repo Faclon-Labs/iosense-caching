29f7a9260d3a963fbf90f75d10f8bc18030a3ff6
dhrutim <dhruti.m@iosense.io>
2023-03-01 14:03:51 +0530

    removed file setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name = "iosense",
-    version = "0.0.1",
-    author = "Dhruti Mistry",
-    description = "iosense data fetching library",
-    packages = ["iosense"]
-)
